f1276136481d216720f7572f0a23f294a99cc926
xutaoding <xutao.ding@chinascopefinancial.com>
2016-06-06 13:23:42 +0800

    add author to each program

--- crawler/cnindex/crawl.py
+++ crawler/cnindex/crawl.py
@@ -1,4 +1,5 @@
 # coding=utf-8
+# author: shuqing.zhou
 import os
 import time
 from urllib import urlretrieve
--- crawler/szse/index_sample.py
+++ crawler/szse/index_sample.py
@@ -1,4 +1,6 @@
 # -*- coding:utf-8 -*-
+# author: peng.wu
+
 from __future__ import unicode_literals
 import lxml.html
 import re
