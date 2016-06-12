3ae53dee79c1a65b3166bca613958c9719b726bf
xutaoding <xutao.ding@chinascopefinancial.com>
2016-06-12 15:39:43 +0800

    fix bugs

--- run.py
+++ run.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 import os
+from datetime import datetime
 from os.path import dirname, abspath
 
+from pymongo import MongoClient
 from scrapy.crawler import CrawlerProcess
 from apscheduler.jobstores.sqlalchemy import SQLAlchemyJobStore
 from apscheduler.schedulers.blocking import BlockingScheduler
@@ -9,6 +11,7 @@ from apscheduler.executors.pool import ThreadPoolExecutor
 
 from crawler import *
 from conf import logger
+from conf import CAN_HOST, CAN_PORT, CAN_DB, CAN_COLLECTION
 from utils.util import StorageMongo
 
 
@@ -37,24 +40,63 @@ job_defaults = {
 app = BlockingScheduler(jobstores=jobstores, executors=executors, job_defaults=job_defaults)
 
 
+def is_workday():
+    today = datetime.now().strftime('%Y')
+    client = MongoClient(CAN_HOST, CAN_PORT)
+    collection = client[CAN_DB][CAN_COLLECTION]
+
+    calendars = {docs['dt'] for docs in collection.find({}, {'dt': 1}) if docs['dt'].startswith(today)}
+    client.close()
+    return calendars
+
+
 @app.scheduled_job(trigger='cron', hour='17')
-def crawl_index_jobs():
+def crawl_sse_index():
+    today = datetime.now().strftime('%Y-%m-%d')
+
     try:
+        if today not in is_workday():
+            return
+
         SSEIndex().crawl()  # 上海交易所指数
     except Exception as e:
         logger.info('SSE crawl error: type <{typ}>, msg <{msg}>'.format(typ=e.__class__, msg=e))
 
+
+@app.scheduled_job(trigger='cron', hour='17')
+def crawl_szse_index():
+    today = datetime.now().strftime('%Y-%m-%d')
+
     try:
+        if today not in is_workday():
+            return
+
         SZSEIndex().upload()  # 深圳交易所指数
     except Exception as e:
         logger.info('SZSE crawl error: type <{typ}>, msg <{msg}>'.format(typ=e.__class__, msg=e))
 
+
+@app.scheduled_job(trigger='cron', hour='17')
+def crawl_cn_index():
+    today = datetime.now().strftime('%Y-%m-%d')
+
     try:
+        if today not in is_workday():
+            return
+
         CNIndex().main()  # CNINDEX 网站指数
     except Exception as e:
         logger.info('CNindex crawl error: type <{typ}>, msg <{msg}>'.format(typ=e.__class__, msg=e))
 
+
+@app.scheduled_job(trigger='cron', hour='17')
+def crawl_cs_index():
+    today = datetime.now().strftime('%Y-%m-%d')
+
     try:
+        if today not in is_workday():
+            return
+
         # 中证指数网站
         cp = CrawlerProcess()
         cp.crawl(CsindexSpider())
