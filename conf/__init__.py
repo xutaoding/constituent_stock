2ef0a971e516c846352f520558698ea2a08d1ca7
xutaoding <xutao.ding@chinascopefinancial.com>
2016-07-05 16:06:51 +0800

    修改.gitignore文件和日志函数

--- conf/__init__.py
+++ conf/__init__.py
@@ -1,17 +1,39 @@
+import os
 import logging
+from logging.handlers import RotatingFileHandler
+from os.path import abspath, dirname, join, exists
 
 from config import *
 
 
-def get_logger():
-    logger = logging.getLogger(__name__)
-    logger.setLevel(logging.INFO)
+def get_logger(to_console=True, to_file=False):
+    log_path = join(dirname(dirname(abspath(__file__))), 'log')
 
-    console = logging.StreamHandler()
-    console.setLevel(logging.INFO)
-    console.setFormatter(logging.Formatter('[%(asctime)s %(levelname)s]: %(message)s'))
+    if not exists(log_path):
+        os.makedirs(log_path)
+    filename = join(log_path, 'crawler.log')
 
-    logger.addHandler(console)
-    return logger
+    _logger = logging.getLogger(__name__)
+    _logger.setLevel(logging.INFO)
 
-logger = get_logger()
+    if to_console:
+        console = logging.StreamHandler()
+        console.setLevel(logging.INFO)
+        console.setFormatter(logging.Formatter('[%(asctime)s %(levelname)s]: %(message)s'))
+
+        _logger.addHandler(console)
+
+    if to_file:
+        # RotatingFileHandler need parameters
+        file_handler = RotatingFileHandler(filename, backupCount=10, maxBytes=5 * 1024 * 1024)
+        file_handler.setLevel(logging.DEBUG)
+
+        datefmt = '%Y-%m-%d %H:%M:%S'
+        formatter = '[%(asctime)s %(levelname)s]: %(message)s'
+        formatter = logging.Formatter(formatter, datefmt)
+        file_handler.setFormatter(formatter)
+
+        _logger.addHandler(file_handler)
+    return _logger
+
+logger = get_logger(to_console=False, to_file=True)
